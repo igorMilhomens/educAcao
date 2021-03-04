ed78003375d9f4cc479a775dfd21ef10d1f0c1d7
Esteban <gustavoesteban13@hotmail.com>
2021-03-04 14:40:56 -0300

    refactor: refatorando UsuarioRepository #11

--- educAcao/educAcao/src/main/java/org/generation/educAcao/repository/UsuarioRepository.java
+++ educAcao/educAcao/src/main/java/org/generation/educAcao/repository/UsuarioRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository <Usuario, Long>{
 	
-	public List<Usuario> findAllByNomeCompletoContainingIgnoreCase( String nomeCompleto);
+	public List<Usuario> findAllByEmailContainingIgnoreCase( String email);
 }
