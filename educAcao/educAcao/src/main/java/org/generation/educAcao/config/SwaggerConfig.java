6704ccf2dc063e2e72d7fd4ea1382de34bb9faea
Felipe Casagrande <fehcasa@hotmail.com>
2021-03-08 11:44:15 -0300

    feat: Implementação da ApiInfo #24

--- educAcao/educAcao/src/main/java/org/generation/educAcao/config/SwaggerConfig.java
+++ educAcao/educAcao/src/main/java/org/generation/educAcao/config/SwaggerConfig.java
@@ -3,8 +3,10 @@ package org.generation.educAcao.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,5 +20,14 @@ public class SwaggerConfig {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors.basePackage("org.generation.educAcao.controller"))
 				.paths(PathSelectors.any()).build().apiInfo(apiInfo());
+		}
+	private ApiInfo apiInfo(){
+		return new ApiInfoBuilder()
+		.title("EducAção")
+		.description("API do Projeto Integrador - EducAção")
+		.version("1.0")
+		.contact(contact())
+		.build();
+
 		}
 }
