d897ccafe256aa9fb5750d8b5e030fd5cc44b576
Igor M <iggorm@yahoo.com.br>
2021-03-31 14:29:23 -0300

    feat: implementado foto e id no Model UserLogin #28

--- educAcao/educAcao/src/main/java/org/generation/educAcao/model/UserLogin.java
+++ educAcao/educAcao/src/main/java/org/generation/educAcao/model/UserLogin.java
@@ -1,6 +1,8 @@
 package org.generation.educAcao.model;
 
 public class UserLogin {
+	
+	private long id;
 
 	private String nome;
 	
@@ -9,6 +11,18 @@ public class UserLogin {
 	private String senha;
 	
 	private String token;
+	
+	private String foto;
+	
+	
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
 
 	public String getNome() {
 		return nome;
@@ -42,4 +56,13 @@ public class UserLogin {
 		this.token = token;
 	}
 
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	
 }
